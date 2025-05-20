55187a0a39f3605a2ab2f358b5c3b5ec31559930
lucianaSanches <sistemati001@gmail.com>
2025-05-20 11:31:29 -0300

    base de dados corrigida 20.05

---
+++ campeonato.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 20-Maio-2025 às 16:27
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `campeonato`
+--
+CREATE DATABASE IF NOT EXISTS `campeonato` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `campeonato`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogadores`
+--
+
+DROP TABLE IF EXISTS `jogadores`;
+CREATE TABLE `jogadores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `posicao` varchar(20) DEFAULT NULL,
+  `idade` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `times`
+--
+
+DROP TABLE IF EXISTS `times`;
+CREATE TABLE `times` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `tecnico` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `jogadores`
+--
+ALTER TABLE `jogadores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `times`
+--
+ALTER TABLE `times`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `jogadores`
+--
+ALTER TABLE `jogadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `times`
+--
+ALTER TABLE `times`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
